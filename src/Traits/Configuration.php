207d254fbe4e4bb4294f578a04a739cac2293762
pengzhile <pengzhile@gmail.com>
2018-10-10 18:49:58 +0800

    update
    Signed-off-by: pengzhile <pengzhile@gmail.com>

--- src/Traits/Configuration.php
+++ src/Traits/Configuration.php
@@ -9,23 +9,14 @@ trait Configuration
 {
     use Injection;
 
+    /**
+     * @var array
+     */
+    protected $config = [];
+
     public function __inject()
     {
-        do {
-            if (empty($this->config)) {
-                break;
-            }
-
-            $config = [];
-            foreach ($this->config as $item => $map) {
-                $map || $map = $item;
-
-                $config[$map] = Config::get($item);
-            }
-
-            $this->config = $config;
-        } while (false);
-
+        $this->config = Config::get('injection.' . __CLASS__, []);
         $this->__injected = true;
     }
 }
